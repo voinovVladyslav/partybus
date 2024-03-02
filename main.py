42262ba3a0408c957f63bda894949250d5ce82ac
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-02 18:26:09 +0200

    feat: load bandwords from txt

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from docx import Document
 
 from service.excel import read_excel, aggregate_data
+from service.banwords import load_banwords
 from service.writers.factory import get_writer
 
 
 data = read_excel('examples/test-partybusfremont.com.xlsx')
 data = aggregate_data(data)
 
-banwords = ['bus']
+banwords = load_banwords('banwords.txt')
+print(f'Loaded {len(banwords)} banwords')
 
 document = Document()
 for i, page_data in enumerate(data['pages'], 1):
@@ -20,7 +22,6 @@ for i, page_data in enumerate(data['pages'], 1):
     writer = get_writer(i, **kwargs)
     print(f'Writing page {1} using {writer.__class__.__name__}...')
     writer.write()
-    break
 
 print('Saving document...')
 document.save('result/partybusfremont.docx')
--- service/banwords.py
+++ service/banwords.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+
+
+def load_banwords(file_path: Path) -> list[str]:
+    with open(file_path, 'r') as file:
+        return [line.strip() for line in file.readlines()]
+
+
 def is_banword(word: str, banwords: list[str]) -> bool:
     for banword in banwords:
         if word.startswith(banword):
