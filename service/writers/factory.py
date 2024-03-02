d22bc3a65c426c681dbf0800f85f13752c92b27f
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-02 17:22:19 +0200

    feat: about page writer

--- service/writers/factory.py
+++ service/writers/factory.py
@@ -8,6 +8,7 @@ from .locations import LocationPageWriter
 from .mainservice import MainServicePageWriter
 from .service import ServicePageWriter
 from .pricing import PricingPageWriter
+from .about import AboutPageWriter
 
 
 def get_writer(document, data: dict, page_number: int) -> BasePageWriter:
@@ -29,5 +30,7 @@ def get_writer(document, data: dict, page_number: int) -> BasePageWriter:
         return ServicePageWriter(document, data)
     if page_type == PageType.PRICING:
         return PricingPageWriter(document, data)
+    if page_type == PageType.ABOUT:
+        return AboutPageWriter(document, data)
 
     raise ValueError(f'Unknown page type {page_type}')
