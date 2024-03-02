33964724b444b06b4b75791575168a6c957717de
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-02 17:11:44 +0200

    feat: locations page writer

--- service/writers/factory.py
+++ service/writers/factory.py
@@ -4,6 +4,7 @@ from .partybus import PartyBusPageWriter
 from .charterbus import CharterBusPageWriter
 from .busfleet import BusFleetPageWriter
 from .bus import BusPageWriter
+from .locations import LocationPageWriter
 
 
 def get_writer(document, data: dict, page_number: int) -> BasePageWriter:
@@ -17,5 +18,7 @@ def get_writer(document, data: dict, page_number: int) -> BasePageWriter:
         return BusFleetPageWriter(document, data)
     if page_type == PageType.BUS:
         return BusPageWriter(document, data)
+    if page_type == PageType.LOCATIONS:
+        return LocationPageWriter(document, data)
 
     raise ValueError(f'Unknown page type {page_type}')
