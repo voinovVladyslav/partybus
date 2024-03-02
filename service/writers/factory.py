62ed87343baa91d59fad732f64ecbed1188a5f8c
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-02 17:10:09 +0200

    feat: bus page writer

--- service/writers/factory.py
+++ service/writers/factory.py
@@ -2,6 +2,8 @@ from .rules import PageType, get_page_type
 from .base import BasePageWriter
 from .partybus import PartyBusPageWriter
 from .charterbus import CharterBusPageWriter
+from .busfleet import BusFleetPageWriter
+from .bus import BusPageWriter
 
 
 def get_writer(document, data: dict, page_number: int) -> BasePageWriter:
@@ -11,5 +13,9 @@ def get_writer(document, data: dict, page_number: int) -> BasePageWriter:
         return PartyBusPageWriter(document, data)
     if page_type == PageType.CHARTER_BUT:
         return CharterBusPageWriter(document, data)
+    if page_type == PageType.BUS_FLEET:
+        return BusFleetPageWriter(document, data)
+    if page_type == PageType.BUS:
+        return BusPageWriter(document, data)
 
     raise ValueError(f'Unknown page type {page_type}')
