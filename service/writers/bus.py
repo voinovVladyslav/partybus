05e888f9d135d3342b44a004a8ad4774b76c73a7
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-02 17:24:45 +0200

    fix: write title takes 1 argument

--- service/writers/bus.py
+++ service/writers/bus.py
@@ -3,7 +3,7 @@ from .base import BasePageWriter
 
 class BusPageWriter(BasePageWriter):
     def write(self) -> None:
-        self.write_title(self.data['name'], 0)
+        self.write_title(self.data['name'])
         for i, row in enumerate(self.data['rows'], 1):
             if i == 1:
                 self.write_heading(row['heading'], 1)
