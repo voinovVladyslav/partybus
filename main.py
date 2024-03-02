fdf295881e7a9b63723cc4efe7c5bef487aa100f
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-02 16:05:18 +0200

    feat: test document

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from docx import Document
+
 from service.excel import read_excel, aggregate_data
 
 
@@ -5,3 +7,16 @@ data = read_excel('examples/test-partybusfremont.com.xlsx')
 data = aggregate_data(data)
 print('phone:', data['phone'])
 print('pages:', len(data['pages']))
+
+for page in data['pages']:
+    pass
+
+document = Document()
+document.add_heading('Party Bus Fremont', level=0)
+document.add_heading('Heading 1', level=1)
+document.add_paragraph('Paragraph 1')
+document.add_page_break()
+document.add_heading('Party Bus Fremont', level=0)
+document.add_heading('Heading 1', level=1)
+document.add_paragraph('Paragraph 1')
+document.save('result/partybusfremont.docx')
