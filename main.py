073158ad2cac0013004134633817f6f864cdad20
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-03 07:25:16 +0200

    feat: input groupbox

--- main.py
+++ main.py
@@ -28,7 +28,11 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.configure_window()
+        self.input_file_path = None
         layout = QGridLayout()
+
+        input_groupbox = self.configure_input_groupbox()
+        layout.addWidget(input_groupbox)
 
         logs_groupbox = self.configure_logs_groupbox()
         layout.addWidget(logs_groupbox)
@@ -38,10 +42,37 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
         self.info('Application started')
 
-
     def configure_window(self):
         self.setFixedSize(QSize(400, 500))
         self.setWindowTitle('Party Bus v1.0')
+
+    def configure_input_groupbox(self):
+        groupbox = QGroupBox('Input')
+        grid_layout = QGridLayout()
+        groupbox.setLayout(grid_layout)
+
+        self.input_file_text = QLineEdit()
+        self.input_file_text.setReadOnly(True)
+        grid_layout.addWidget(QLabel('Input:'), 0, 0, 1, 1)
+        grid_layout.addWidget(self.input_file_text, 0, 1, 1, 2)
+
+        self.input_button = QPushButton('Select File')
+        self.input_button.clicked.connect(self.handle_input_button)
+        grid_layout.addWidget(self.input_button, 0, 3, 1, 1)
+
+        return groupbox
+
+    def handle_input_button(self):
+        file_path = self.get_file_path(
+            caption='Select Data File',
+            filter='Excel Files (*.xlsx *.xls *.xlsm *.xlsb)',
+        )
+        if not file_path:
+            self.info('No input file selected')
+            return
+        self.input_file_path = file_path
+        self.input_file_text.setText(file_path)
+        self.info(f'Selected input file: {file_path}')
 
     def configure_logs_groupbox(self):
         groupbox = QGroupBox('Logs')
