804cd2ffe5bdededaf59f2de386ca05d5ad19c08
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-03 19:23:41 +0200

    feat: pre load banwords file

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from docx import Document
 from PyQt6.QtCore import (
     QSize,
+    QSettings,
 )
 from PyQt6.QtWidgets import (
     QApplication,
@@ -24,6 +25,9 @@ from service.banwords import load_banwords
 from service.excel import read_excel, aggregate_data
 from service.writers.factory import get_writer
 
+COMPANY_NAME = 'Party Bus'
+APPLICATION_NAME = 'Party Bus v1.0'
+
 logger = logging.getLogger('main')
 
 
@@ -31,6 +35,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.configure_window()
+        self.settings = QSettings(COMPANY_NAME, APPLICATION_NAME)
 
         self.input_file_path = None
         self.banwords_file_path = None
@@ -56,6 +61,7 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
         self.setCentralWidget(widget)
         self.info('Application started')
+        self.preload_banwords()
 
     def configure_window(self):
         self.setFixedSize(QSize(400, 400))
@@ -135,6 +141,7 @@ class MainWindow(QMainWindow):
         groupbox.setLayout(grid_layout)
 
         self.banwords_input_text = QLineEdit()
+
         self.banwords_input_text.setReadOnly(True)
         grid_layout.addWidget(QLabel('Banwords:'), 0, 0, 1, 1)
         grid_layout.addWidget(self.banwords_input_text, 0, 1, 1, 2)
@@ -146,6 +153,22 @@ class MainWindow(QMainWindow):
         grid_layout.addWidget(self.select_bandwords_button, 0, 3, 1, 1)
 
         return groupbox
+
+    def preload_banwords(self):
+        try:
+            prev_banwords_file_path = self.settings.value('banwords_file_path')
+            if not prev_banwords_file_path:
+                self.info('No previous banwords file found')
+                return
+            self.info(
+                f'Loading previous banwords file {prev_banwords_file_path}'
+            )
+            self.banwords_input_text.setText(prev_banwords_file_path)
+            self.banwords_file_path = prev_banwords_file_path
+            self.banwords = load_banwords(prev_banwords_file_path)
+            self.info(f'Loaded {len(self.banwords)} banwords')
+        except Exception:
+            self.info('Faled to load previous banwords file')
 
     def handle_select_bandwords_button(self):
         file_path = self.get_file_path(
@@ -159,6 +182,7 @@ class MainWindow(QMainWindow):
         self.banwords_input_text.setText(file_path)
         self.info(f'Selected banwords file: {file_path}')
 
+        self.settings.setValue('banwords_file_path', file_path)
         self.banwords = load_banwords(file_path)
         self.info(f'Loaded {len(self.banwords)} banwords')
 
