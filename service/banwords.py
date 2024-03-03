805376c8571284a05e7d5404cb3cf3dfeb68fd8a
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-03-03 12:13:05 +0200

    fix: lower banwords

--- service/banwords.py
+++ service/banwords.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def load_banwords(file_path: Path) -> set[str]:
     with open(file_path, 'r') as file:
-        return set([line.strip() for line in file.readlines()])
+        return set([line.strip().lower() for line in file.readlines()])
 
 
 def is_banword(word: str, banwords: list[str]) -> bool:
