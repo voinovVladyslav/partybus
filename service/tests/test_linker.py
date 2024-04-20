700f9ce2e1609c3743db3e9b0c913e9e95f2563c
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-04-20 19:34:05 +0300

    test: linker replaceability

---
+++ service/tests/test_linker.py
@@ -0,0 +1,62 @@
+from service.links.base import Linker
+
+
+def test_linker_single_match():
+    patterns = [
+        {
+            'header': 'Home',
+            'link': '/home/',
+            'keywords': ['country', 'home']
+        }
+    ]
+    linker = Linker(patterns)
+    text = 'This is my country'
+    rendered_text = linker.render(text)
+    assert rendered_text == 'This is my <a href="/home/">country</a>'
+
+
+def test_linker_multiple_matches():
+    patterns = [
+        {
+            'header': 'Home',
+            'link': '/home/',
+            'keywords': ['country', 'home']
+        }
+    ]
+    linker = Linker(patterns)
+    text = 'This country is my home'
+    rendered_text = linker.render(text)
+    assert rendered_text == 'This <a href="/home/">country</a> is my <a href="/home/">home</a>'  # noqa
+
+
+def test_linker_multiple_patterns_multiple_matches():
+    patterns = [
+        {
+            'header': 'Home',
+            'link': '/home/',
+            'keywords': ['home']
+        },
+        {
+            'header': 'Country',
+            'link': '/country/',
+            'keywords': ['country']
+        }
+    ]
+    linker = Linker(patterns)
+    text = 'This country is my home'
+    rendered_text = linker.render(text)
+    assert rendered_text == 'This <a href="/country/">country</a> is my <a href="/home/">home</a>'  # noqa
+
+
+def test_linker_single_match_with_overlaping_keywords():
+    patterns = [
+        {
+            'header': 'Home',
+            'link': '/home/',
+            'keywords': ['home country', 'home']
+        }
+    ]
+    linker = Linker(patterns)
+    text = 'This is my home country'
+    rendered_text = linker.render(text)
+    assert rendered_text == 'This is my <a href="/home/">home country</a>'
