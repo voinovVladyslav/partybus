9444bcf5d67c797203f2333181ec945d69dc7f38
Voinov Vladyslav <voinovvladyslav@gmail.com>
2024-04-20 20:17:46 +0300

    feat: home page linker

---
+++ service/links/home.py
@@ -0,0 +1,20 @@
+from .base import Linker
+
+
+class HomeLinker(Linker):
+    def filter_patterns(self) -> None:
+        headers = [
+            'home_page',
+            '18_passenger_minibus',
+            '20_passenger_minibus',
+            '25_passenger_minibus',
+            'field_trip',
+            'wedding',
+            'corporate',
+            'sports',
+            'city_charter_bus',
+        ]
+        self.patterns = [
+            pattern for pattern in self.patterns if
+            pattern['header'].lower() in headers
+        ]
